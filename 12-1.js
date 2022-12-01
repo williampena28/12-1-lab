c446a10a7a77d964142fa85917a6f6398969fec9
williampena28 <56842242+williampena28@users.noreply.github.com>
2022-12-01 13:46:23 -0500

    B 1,2

---
+++ 12-1.js
@@ -0,0 +1,22 @@
+//William Peña
+
+/*
+    A.      Q + A
+1. How do we assign a value to a variable? 
+    A. We use the assignment operator '=' and then the value after it
+2. How do we change the value of a variable? 
+    A. We use the assignment operator '=' and then the new value of the variable
+3. How do we assign an existing variable to a new variable? 
+    A. We use the assignment operator '='after the name of the new variable then the name of the existing variable afterward
+4. Remind me, what are declare, assign, and define? 
+    A. Declaring is telling the program that we need said variable that we are declaring. Assigning is giving a value to a  using the '=' operator. Defining a variable is asigning its value to the correct data type (i.e string, num, float, null)
+5. What is pseudocoding and why should you do it? 
+    A. Pseudocoding is the process of writing out your process for what your program is going to do, step by step in plain english so that you get a thoughtout idea for how your code works.
+6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it 
+    A. Around 90% of time should be spent thinking out and understanding the problem
+*/
+
+//    B.      STRINGS
+//1. Create a variable called firstVariable
+//2. 
+let firstVariable = "Hellow World!";
